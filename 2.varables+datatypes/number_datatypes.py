198c77af24c5b309ad025727a97e4e91e7052b0a
Jaewon2468 <mbc0296@naver.com>
2021-07-22 16:59:46 +0900

    first commit

---
+++ 2.varables+datatypes/number_datatypes.py
@@ -0,0 +1,24 @@
+a=154
+print(type(a))
+a=0
+print(type(a))
+a=-25
+print(type(a))
+#실수형
+b=181.34
+print(type(b))
+b=-22.22
+print(type(b))
+#복수수형
+c=1+4j
+print(type(c))
+print(c.real)
+print(c.imag)
+print(c.conjugate())
+print(abs(c)) #루트(1 + 4*4 =17) = 4.12XXX
+
+#예제
+a=4
+b=3.14
+c=3+4j
+print(10*a + 3*b + c)
---
+++ 2.varables+datatypes/week1_2018113102_통계학과_서재완.py
@@ -0,0 +1,9 @@
+# 개인정보 및 목표
+name = "서재완"
+major = "통계학과"
+grade = "2학년"
+goal = "교육을 열심히 듣고 파이썬과 프로그래밍에 관해 많은 것을 배우고 싶습니다"
+
+print(f'경북대학교 {major} {grade} {name}은 이번 sw융합교육을 통해\n{goal}')
+
+#print("경북대학교 {0} {1} {2}은 이번 sw융합교육을 통해\n{3}".format(major,grade,name,goal))
